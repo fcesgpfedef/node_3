9851d3316d3d591f17db72b36702547fd657a08a
fcesgpfedef <35006774+fcesgpfedef@users.noreply.github.com>
2022-11-18 08:54:29 +0530

    Create mongoOperations.js

---
+++ mongoOperations.js
@@ -0,0 +1,24 @@
+const {MongoClient} = require('mongodb'); // imported the mongoClient
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const connection = async () => {
+    try {
+        await client.connect();// waiting untill connection in established.
+        const collection = client.db('node_3').collection('employee');
+        await collection.insertOne({name:'Yashwanth', city : 'GUntur', course:'Nodejs' });
+        const findResult = await collection.findOne({name:'Yashwanth'});
+        await collection.updateOne({name:'Yashwanth'}, {$set:{city:'Vijaywada'}});
+        const findResultAfterUpd = await collection.findOne({name:'Yashwanth'});
+        const number = await collection.countDocuments();
+        return Promise.resolve({findResult, findResultAfterUpd, numberOfRecords:number});
+    }catch(err){
+        return Promise.reject(err);
+    }
+};
+
+connection().then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.log(`Some Error ${error}`);
+});
